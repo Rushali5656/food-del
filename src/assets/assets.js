cf7f7805b5c6362759abcac2d35deeb7f96b77fc
Rushali5656 <rushalilage23@gmail.com>
2024-07-12 19:02:05 +0545

    add

---
+++ src/assets/assets.js
@@ -0,0 +1,84 @@
+import food from "./food.png";
+import cart from "./cart.png";
+import search from "./search.png";
+import salad from "./salad.jpg";
+import burger from "./burger.jpg";
+import sandwich from "./sandwich.jpg";
+import pizza from "./pizza.png";
+import add from "./add.png";
+import plus from "./plus.png";
+import minus from "./minus.png";
+export const assets = {
+  food,
+  cart,
+  search,
+  add,
+  plus,
+  minus,
+};
+export const menu_list = [
+  { menu_name: "Salad", menu_image: sandwich },
+  { menu_name: "Rolls", menu_image: burger },
+  { menu_name: "Deserts", menu_image: salad },
+  { menu_name: "Sandwich", menu_image: pizza },
+  { menu_name: "Burger", menu_image: burger },
+  { menu_name: "Pizza", menu_image: salad },
+  { menu_name: "Fry Rice", menu_image: sandwich },
+  { menu_name: "Burger", menu_image: burger },
+];
+export const food_list = [
+  {
+    _id: "1",
+    name: "Greek salad",
+    image: burger,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being.",
+    category: "Salad",
+  },
+  {
+    _id: "2",
+    name: "Greek salad",
+    image: sandwich,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being.",
+    category: "Salad",
+  },
+  {
+    _id: "3",
+    name: "Greek salad",
+    image: salad,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being.",
+    category: "Salad",
+  },
+  {
+    _id: "4",
+    name: "Greek salad",
+    image: pizza,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being.",
+    category: "Salad",
+  },
+  {
+    _id: "5",
+    name: "Greek salad",
+    image: salad,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being.",
+    category: "Salad",
+  },
+  {
+    _id: "6",
+    name: "Greek salad",
+    image: burger,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being.",
+    category: "Salad",
+  },
+];
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+// import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { useState } from 'react'
+
+function Navbar() {
+  const [menu,setMenu]=useState("home")
+  return (
+    <div className='navbar'>
+
+      <img src={assets.food} alt='Food' className='logo'/>
+      <ul className="navbar-menu">
+        <li  onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+      <li  onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+      <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
+      <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li></ul>
+      <div className="navbar-right">
+        <img src={assets.search} alt='search' className='icon'/>
+        <div className="navbar-search-icon">
+          <img src={assets.cart} alt='cart' className='icon'/>
+          <div className='dot'></div>
+         
+         
+        </div>
+        <button>Sign In</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
